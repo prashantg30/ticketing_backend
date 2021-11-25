438dc9cd47a65ecdee9756930cb11093d366064f
prashantg30 <prashant.gupta@cloudstok.com>
2021-11-25 14:40:16 +0530

    Changes by Vishal

--- controllers/controller_login.js
+++ controllers/controller_login.js
@@ -12,31 +12,6 @@ const login = async (req,res,next) =>{
 
     try{
 
-    //     await conn.query(
-    //         "SELECT * FROM `users` WHERE `email`=?",
-    //         [req.body.email]
-    //       );
-
-
-    //     if (row.length === 0) {
-    //         return res.status(422).json({
-    //             message: "Invalid email address",nodemon
-    //         });
-    //     }
-
-    //     const passMatch = await bcrypt.compare(req.body.password, row[0].password);
-    //     if(!passMatch){
-    //         return res.status(422).json({
-    //             message: "Incorrect password",
-    //         });
-    //     }
-
-    //     const theToken = jwt.sign({id:row[0].id},'the-super-strong-secrect',{ expiresIn: '1h' });
-
-    //     return res.json({
-    //         token:theToken
-    //     });
-
     await conn.query(`select * from  user where email ='${req.body.email}'`,(err,result, Failed
         )=>{console.log(result)
             if(result.length ===1 ){ 
@@ -44,7 +19,21 @@ const login = async (req,res,next) =>{
           console.log(row.password)
           console.log(req.body.password)
           bcrypt.compare(req.body.password, row.password, (err, result)=> {
-          console.log(result)
+         console.log(result)
+          if(result === true){
+            jwt.sign({id:row.id},'the-super-strong-secrect',{ expiresIn: '1h' },(err,result)=>{
+                if(!err){
+                    console.log(result)
+                    return res.json({
+                             token:result
+                         });
+                }
+            });
+          }
+          if(!result === true)
+          {
+            return res.status(422).json({ message: "Incorrect password",});
+          }
         });
 
             }else {
