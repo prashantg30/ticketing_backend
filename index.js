49b348dd8966d3a304871e6d5e2cef8a8a4c89d7
prashantg30 <prashant.gupta@cloudstok.com>
2021-11-25 12:03:30 +0530

    script added

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ conn.connect((err)=>{
     }
 });
 app.use(route);
+app.get('/', (req, res) =>{
+    res.send('Server is Running');
+})
 app.listen(3002, ()=>console.log("Server is listening at port: 3002"));
 
 
